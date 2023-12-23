1c2860b888e462b2fd889881a82e8a438d5c6fbd
Vikramjeet Singh <vikramjeet@founderandlightning.com>
2023-12-23 10:38:05 +0530

    add test command

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Category;
+use App\Models\User;
+use App\Util\MailchimpUtil;
+use Exception;
+use Illuminate\Console\Command;
+use Log;
+
+class Test extends Command
+{
+    protected $signature = 'test:command';
+
+    protected $description = 'Command to test log'
+
+    public function handle(): bool
+    {
+        Log::info('Testing worker');
+    }
+}
